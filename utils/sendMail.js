9bff4a4f6c365dde0fa13fd2376a0a865bd8f67d
jake <jake.east@gmail.com>
2021-08-08 18:34:42 -0500

    updated email URL

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -17,7 +17,7 @@ module.exports.sendVerifyMail = (email, verifyLink) => {
         to: email,
         subject: 'Please confirm your email address',
         text: 'Hey it worked',
-        html: `<a href="http://localhost:3000/verify/${verifyLink}">Click here to verify your account</a>.  If you do not verify your account within 24 hours, you will have to start the registration process over.`
+        html: `<a href="https://timeline-sandbox.herokuapp.com/verify/${verifyLink}">Click here to verify your account</a>.  If you do not verify your account within 24 hours, you will have to start the registration process over.`
     }
         //calling nodemailer to send the message when the user is registered
         transporter.sendMail(message);
@@ -32,7 +32,7 @@ module.exports.sendResetMail = (email, verifyLink) => {
         to: email,
         subject: 'Password Reset Request',
         text: 'Hey it worked',
-        html: `<a href="http://localhost:3000/forgotpassword/${verifyLink}">Click here to reset your password.</a>  If you did not request this, you may ignore this email or alert an admin.`
+        html: `<a href="https://timeline-sandbox.herokuapp.com/forgotpassword/${verifyLink}">Click here to reset your password.</a>  If you did not request this, you may ignore this email or alert an admin.`
     }
             //calling nodemailer to send the message when the user is registered
         transporter.sendMail(message);
