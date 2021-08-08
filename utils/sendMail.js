9b85f048160f201ec08d2522de9ce3d3b51e5367
jake <jake.east@gmail.com>
2021-08-08 18:26:49 -0500

    Updated email templates

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -15,9 +15,9 @@ module.exports.sendVerifyMail = (email, verifyLink) => {
     const message = {
         from: process.env.EMAIL_USER,
         to: email,
-        subject: 'Test message',
+        subject: 'Please confirm your email address',
         text: 'Hey it worked',
-        html: `<a href="http://localhost:3000/verify/${verifyLink}">Click here to verify your account.  If you do not verify your account within 24 hours, you will have to start the registration process over.</a>`
+        html: `<a href="http://localhost:3000/verify/${verifyLink}">Click here to verify your account</a>.  If you do not verify your account within 24 hours, you will have to start the registration process over.`
     }
         //calling nodemailer to send the message when the user is registered
         transporter.sendMail(message);
@@ -30,9 +30,9 @@ module.exports.sendResetMail = (email, verifyLink) => {
     const message = {
         from: process.env.EMAIL_USER,
         to: email,
-        subject: 'Test message',
+        subject: 'Password Reset Request',
         text: 'Hey it worked',
-        html: `<a href="http://localhost:3000/forgotpassword/${verifyLink}">Click here to reset your password.</a>`
+        html: `<a href="http://localhost:3000/forgotpassword/${verifyLink}">Click here to reset your password.</a>  If you did not request this, you may ignore this email or alert an admin.`
     }
             //calling nodemailer to send the message when the user is registered
         transporter.sendMail(message);
