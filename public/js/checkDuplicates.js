6f6c98f728d30d858680f2930c7c2d94bfab7723
jake <jake.east@gmail.com>
2021-08-30 11:21:15 -0500

    combining javascript functions into one file

--- public/js/checkDuplicates.js
+++ public/js/checkDuplicates.js
@@ -1,3 +1,5 @@
+//TODO: DELETE THIS FILE
+
 export const checkDuplicates = async (data) => {
     const response = await fetch('/sources/data?' + new URLSearchParams({
         title: data.title,
@@ -41,3 +43,56 @@ export class Duplicate {
     }
 }
 
+const countdown = document.querySelector('.countdown-popup')
+const countdownTimer = document.querySelector('.countdown-timer')
+const blurBackground = document.querySelector('.disableDiv')
+const timerButton = document.querySelector('#timerButton')
+
+const startingMinutes = 1.1 //sets timeout for page
+const warningTime = 1 * 60 //sets time when warning will pop up
+let time = startingMinutes * 60 //timer for use in idleLogout function, should not change
+
+
+export const idleLogout = () => { //function for kicking user out of the page if they don't take any action
+
+    const closePopup = () => { //closes the warning popup and resets everything
+        countdown.style.display = 'none'
+        blurBackground.style.display = 'none'
+        time = startingMinutes * 60
+    }
+    
+    const openPopup = () => { //opens a popup at the warning time to tell user they will be kicked out
+        countdownTimer.innerHTML = `${minutes}:${seconds}`
+        countdown.style.display = 'block'
+        blurBackground.style.display = 'block'
+        timerButton.addEventListener('click', closePopup)
+    }
+
+    const resetTimer = () => {
+        if (time > warningTime) {
+            time = startingMinutes * 60
+        }
+    }
+    
+    const minutes = Math.floor(time / 60)
+    let seconds = time % 60
+    seconds = seconds < 10 ? '0' + seconds : seconds
+
+    if (time <= warningTime) {
+        openPopup()
+    }
+
+    if (time <= 0) {
+        location.href = '/dashboard'
+    }
+
+    console.log(minutes, seconds)
+    time--
+    window.onload = resetTimer;
+    window.onmousemove = resetTimer;
+    window.onmousedown = resetTimer;  // catches touchscreen presses as well      
+    window.ontouchstart = resetTimer; // catches touchscreen swipes as well 
+    window.onclick = resetTimer;      // catches touchpad clicks as well
+    window.onkeydown = resetTimer;   
+    window.addEventListener('scroll', resetTimer, true); // improved; see comments
+}
